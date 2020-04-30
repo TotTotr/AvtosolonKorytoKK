b95b61237ba0c96b64e2690160a7786774242b83
Katerina881 <mr.alodov@mail.ru>
2020-04-30 13:45:02 +0400

    1

---
+++ KorytoKirillovaKhisamov/KorytoModel/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace KorytoModel
+{
+    [DataContract]
+    public class Car
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        [Required]
+        public string CarName { get; set; }
+
+        [Required]
+        [DataMember]
+        public decimal Price { get; set; }
+
+        [Required]
+        [DataMember]
+        public int Year { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual List<CarDetail> CarDetails { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual List<OrderCar> OrderCars { get; set; }
+    }
+}
---
+++ KorytoKirillovaKhisamov/KorytoModel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("KorytoModel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KorytoModel")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("daf77f88-a1d7-4a15-a3b5-52c6b678bdb1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
